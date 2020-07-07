5dd801bbdaad7ca9fbdbbc71bb9798e1939dea35
Stefan Reimer <stefan@zero-downtime.net>
2020-07-07 13:17:20 +0100

    Add options migration to calico

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -14,6 +14,7 @@ helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm repo add argoproj https://argoproj.github.io/argo-helm
 helm repo add jetstack https://charts.jetstack.io
 helm repo add uswitch https://uswitch.github.io/kiam-helm-charts/charts/
+helm repo update
 
 for dir in $(find $SRCROOT/charts -mindepth 1 -maxdepth 1 -type d);
 do
@@ -21,15 +22,9 @@ do
 
     if [ $(helm dep list $dir 2>/dev/null| wc -l) -gt 1 ]
     then
-        # Bug with Helm subcharts with hyphen on them
-        # https://github.com/argoproj/argo-helm/pull/270#issuecomment-608695684
-        if [ "$name" == "argo-cd" ]
-        then
-            echo "Restore ArgoCD RedisHA subchart"
-            git checkout $dir
-        fi
         echo "Processing chart dependencies"
-        helm --debug dep build $dir
+        rm -rf $dir/tmpcharts
+        helm dependency update --skip-refresh $dir
     fi
 
     echo "Processing $dir"
