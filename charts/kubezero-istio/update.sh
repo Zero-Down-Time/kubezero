cd5b38bb6cbadac4646027a4e9c2cfe37f26744e
Stefan Reimer <stefan@zero-downtime.net>
2020-10-02 10:38:09 -0700

    Istio version bump, make http10 support optional, enable redis,mysql protocol support

--- charts/kubezero-istio/update.sh
+++ charts/kubezero-istio/update.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 set -ex
 
-ISTIO_VERSION=1.7.1
+export ISTIO_VERSION=1.7.3
 
-NAME="istio-$ISTIO_VERSION"
-URL="https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-linux-amd64.tar.gz"
+if [ ! -d istio-$ISTIO_VERSION ]; then
+  NAME="istio-$ISTIO_VERSION"
+  URL="https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istio-${ISTIO_VERSION}-linux-amd64.tar.gz"
 
-curl -sL "$URL" | tar xz
+  curl -sL "$URL" | tar xz
+fi
 
 # Now lets extract what we need
 rm -rf charts/istio-operator
 cp -r istio-${ISTIO_VERSION}/manifests/charts/istio-operator charts
 
-rm -rf istio-${ISTIO_VERSION}
-
 # Apply our patch
 patch  -i istio-operator.patch -p0
 
+[ -x istioctl ] || { curl -sL https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istioctl-${ISTIO_VERSION}-linux-amd64.tar.gz | tar xz; chmod +x istioctl; }
+
 # Extract base / CRDs from istioctl into plain manifest to workaround chicken egg problem with CRDs
-istioctl manifest generate --set profile=empty --set components.base.enabled=true > templates/istio-base.yaml
+./istioctl manifest generate --set profile=empty --set components.base.enabled=true > templates/istio-base.yaml
