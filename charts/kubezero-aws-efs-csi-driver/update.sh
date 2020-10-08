54335c4c0a5ef87e6a96385ae1589205afed342f
Stefan Reimer <stefan@zero-downtime.net>
2020-10-08 07:52:34 -0700

    Update EFS tooling to track releases

--- charts/kubezero-aws-efs-csi-driver/update.sh
+++ charts/kubezero-aws-efs-csi-driver/update.sh
@@ -2,9 +2,11 @@
 set -ex
 
 REPO="kubernetes-sigs/aws-efs-csi-driver"
-LATEST_RELEASE=$(curl -sL -s https://api.github.com/repos/${REPO}/releases | grep '"tag_name":' | cut -d'"' -f4 | grep -v -E "(alpha|beta|rc)" | sort -t"." -k 1,1 -k 2,2 -k 3,3 -k 4,4 | tail -n 1)
+[ -z "$RELEASE" ] && RELEASE=$(curl -sL -s https://api.github.com/repos/${REPO}/releases | grep '"tag_name":' | cut -d'"' -f4 | grep -v -E "(alpha|beta|rc)" | sort -t"." -k 1,1 -k 2,2 -k 3,3 -k 4,4 | tail -n 1)
 
-URL="https://github.com/${REPO}/releases/download/${LATEST_RELEASE}/helm-chart.tgz"
+rm -rf git
+git clone https://github.com/$REPO.git git
+cd git && git checkout $RELEASE && cd -
 
 rm -rf charts && mkdir -p charts/aws-efs-csi-driver
-curl -sL "$URL" | tar xz -C charts/aws-efs-csi-driver --strip-components=1
+mv git/helm/* charts/aws-efs-csi-driver
