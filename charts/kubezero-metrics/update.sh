91688607428d28b5bef88dc53e252159b838d26d
Stefan Reimer <stefan@zero-downtime.net>
2021-04-15 15:03:52 +0200

    feat: kube-prometheus-stack version bump, adjust filesytem alerts

--- charts/kubezero-metrics/update.sh
+++ charts/kubezero-metrics/update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-VERSION=14.3.0
+VERSION=14.9.0
 
 rm -rf charts/kube-prometheus-stack
 curl -L -s -o - https://github.com/prometheus-community/helm-charts/releases/download/kube-prometheus-stack-${VERSION}/kube-prometheus-stack-${VERSION}.tgz | tar xfz - -C charts
 
-patch -p3 -i remove_etcd_grpc_alerts.patch --no-backup-if-mismatch
+# The grpc alerts could be re-enabled with etcd 3.5
+# https://github.com/etcd-io/etcd/pull/12196
+patch -p0 -i adjust_alarms.patch --no-backup-if-mismatch
