41f8c6fb5fa234ed985ceff16a6f403fa143e196
Stefan Reimer <stefan@zero-downtime.net>
2021-05-04 15:00:24 +0200

    feat: add file backed persistence and grafana dashboard to NATS

--- charts/kubezero-nats/update.sh
+++ charts/kubezero-nats/update.sh
@@ -7,3 +7,6 @@ rm -rf charts/nats && mkdir -p charts/nats
 git clone --depth=1 https://github.com/nats-io/k8s.git
 cp -r k8s/helm/charts/nats/* charts/nats/
 rm -rf k8s
+
+# Fetch dashboards
+../kubezero-metrics/sync_grafana_dashboards.py dashboards.yaml templates/grafana-dashboards.yaml
