7fcdbfc2cd8756f98438d9532430ebbabab8c54e
Stefan Reimer <stefan@zero-downtime.net>
2021-07-01 16:41:31 +0200

    feat: aws-ebs and aws-efs csi driver upgrades, migrate to use service account tokens for AWS access

--- charts/kubezero-aws-ebs-csi-driver/update.sh
+++ charts/kubezero-aws-ebs-csi-driver/update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VERSION=0.10.0
+VERSION=1.2.3
 
 rm -rf charts/aws-ebs-csi-driver
 curl -L -s -o - https://github.com/kubernetes-sigs/aws-ebs-csi-driver/releases/download/helm-chart-aws-ebs-csi-driver-${VERSION}/aws-ebs-csi-driver-${VERSION}.tgz | tar xfz - -C charts
 
-patch -d charts -i ../loglevel_leader.patch -p0 --no-backup-if-mismatch
+patch -i zdt.patch -p0 --no-backup-if-mismatch
--- charts/kubezero-aws-efs-csi-driver/update.sh
+++ charts/kubezero-aws-efs-csi-driver/update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VERSION=1.2.2
+VERSION=2.1.1
 
 rm -rf charts/aws-efs-csi-driver
 curl -L -s -o - https://github.com/kubernetes-sigs/aws-efs-csi-driver/releases/download/helm-chart-aws-efs-csi-driver-${VERSION}/aws-efs-csi-driver-${VERSION}.tgz | tar xfz - -C charts
 
-patch -i affinity_resources.patch -p0 --no-backup-if-mismatch
+# patch -i zdt.patch -p0 --no-backup-if-mismatch
