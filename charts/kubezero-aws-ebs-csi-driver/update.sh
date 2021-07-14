c52258dd9d833b971882db7a002e29820304bcff
Stefan Reimer <stefan@zero-downtime.net>
2021-07-14 23:36:57 +0200

    Fix: aws-ebs version bump

--- charts/kubezero-aws-ebs-csi-driver/update.sh
+++ charts/kubezero-aws-ebs-csi-driver/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=1.2.3
+VERSION=1.2.4
 
 rm -rf charts/aws-ebs-csi-driver
 curl -L -s -o - https://github.com/kubernetes-sigs/aws-ebs-csi-driver/releases/download/helm-chart-aws-ebs-csi-driver-${VERSION}/aws-ebs-csi-driver-${VERSION}.tgz | tar xfz - -C charts
