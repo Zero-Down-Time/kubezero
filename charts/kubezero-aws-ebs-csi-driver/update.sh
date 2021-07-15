ee60f455582ea2c7b457eedcddf53221a4e81355
Stefan Reimer <stefan@zero-downtime.net>
2021-07-15 12:14:01 +0200

    fix: remove duplicated CRDs

--- charts/kubezero-aws-ebs-csi-driver/update.sh
+++ charts/kubezero-aws-ebs-csi-driver/update.sh
@@ -6,3 +6,6 @@ rm -rf charts/aws-ebs-csi-driver
 curl -L -s -o - https://github.com/kubernetes-sigs/aws-ebs-csi-driver/releases/download/helm-chart-aws-ebs-csi-driver-${VERSION}/aws-ebs-csi-driver-${VERSION}.tgz | tar xfz - -C charts
 
 patch -i zdt.patch -p0 --no-backup-if-mismatch
+
+# Remove duplicated CRDs
+rm -f charts/aws-ebs-csi-driver/templates/crds.yml
