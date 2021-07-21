98ee25a8d17162a7f14530a3cb97d86be3d224c8
Stefan Reimer <stefan@zero-downtime.net>
2021-07-21 15:06:40 +0200

    feat: add prometheus pushgateway as optional component

--- charts/kubezero-metrics/dashboards/build.sh
+++ charts/kubezero-metrics/dashboards/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-jb update
+[ -x ./jb-linux-amd64 ] || wget https://github.com/jsonnet-bundler/jsonnet-bundler/releases/download/v0.4.0/jb-linux-amd64 && chmod +x ./jb-linux-amd64
+
+./jb-linux-amd64 update
 
 mkdir -p kube-mixin
 jsonnet -J vendor -m kube-mixin -e '(import "mixin.libsonnet").grafanaDashboards'
