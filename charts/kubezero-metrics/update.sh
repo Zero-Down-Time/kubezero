a0de753b6380c86d5ae20e1091e4c620b9764d1f
Stefan Reimer <stefan@zero-downtime.net>
2021-07-21 15:06:40 +0200

    feat: add prometheus pushgateway as optional component

--- charts/kubezero-metrics/update.sh
+++ charts/kubezero-metrics/update.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 VERSION=16.13.0
+PG_VER=1.10.1
 
 rm -rf charts/kube-prometheus-stack
-curl -L -s -o - https://github.com/prometheus-community/helm-charts/releases/download/kube-prometheus-stack-${VERSION}/kube-prometheus-stack-${VERSION}.tgz | tar xfz - -C charts
+helm pull prometheus-community/kube-prometheus-stack --untar --untardir charts --version $VERSION
+
+rm -rf charts/prometheus-pushgateway
+helm pull prometheus-community/prometheus-pushgateway --untar --untardir charts --version $PG_VER
 
 # The grpc alerts could be re-enabled with etcd 3.5
 # https://github.com/etcd-io/etcd/pull/12196
 patch -p0 -i zdt.patch --no-backup-if-mismatch
+
+patch -p0 -i zdt-pushgateway.patch --no-backup-if-mismatch
 
 # Create ZDT dashboard configmap
 cd dashboards
