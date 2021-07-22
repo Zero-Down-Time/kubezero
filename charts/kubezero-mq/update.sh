13628ace58e135eb1b9ed7b25abf43c104d44f1e
Stefan Reimer <stefan@zero-downtime.net>
2021-07-22 22:15:12 +0200

    feat: kubezero-mq NATS version bump

--- charts/kubezero-mq/update.sh
+++ charts/kubezero-mq/update.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -ex
 
-### NATS
+## NATS
 
-# get latest chart until they have upstream repo fixed
-rm -rf charts/nats && mkdir -p charts/nats
-
-git clone --depth=1 https://github.com/nats-io/k8s.git
-cp -r k8s/helm/charts/nats/* charts/nats/
-rm -rf k8s
+NATS_VERSION=0.8.4
+rm -rf charts/nats && curl -L -s -o - https://github.com/nats-io/k8s/releases/download/v$NATS_VERSION/nats-$NATS_VERSION.tgz | tar xfz - -C charts
 
 # Fetch dashboards
 ../kubezero-metrics/sync_grafana_dashboards.py dashboards-nats.yaml templates/nats/grafana-dashboards.yaml
