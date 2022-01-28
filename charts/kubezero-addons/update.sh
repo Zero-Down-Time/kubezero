593c296e92e1fe6d70606a79313efbd207cede0e
Stefan Reimer <stefan@zero-downtime.net>
2022-01-28 17:22:12 +0100

    feat: update aws-node-termination handler chart, first version of forseti

--- charts/kubezero-addons/update.sh
+++ charts/kubezero-addons/update.sh
@@ -3,8 +3,9 @@ set -ex
 
 NTH_VERSION=$(yq eval '.dependencies[] | select(.name=="aws-node-termination-handler") | .version' Chart.yaml)
 
-rm -rf charts/aws-node-termination-handler
-helm pull eks/aws-node-termination-handler --untar --untardir charts --version $NTH_VERSION
+# Disabled until these AWS "pros" bump the chart number
+#rm -rf charts/aws-node-termination-handler
+#helm pull eks/aws-node-termination-handler --untar --untardir charts --version $NTH_VERSION
 
 # diff -tuNr charts/aws-node-termination-handler.orig charts/aws-node-termination-handler > nth.patch
 patch -p0 -i nth.patch --no-backup-if-mismatch
