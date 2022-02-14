2c69869deb654b5a10a950a151539c050f06acb9
Stefan Reimer <stefan@zero-downtime.net>
2022-02-14 13:35:13 +0100

    fix: fix quotes, do not execute scan during push

--- vars/buildPodman.groovy
+++ vars/buildPodman.groovy
@@ -49,7 +49,7 @@ def call(Map config=[:]) {
             ]
 
             // Scan again and fail on CRITICAL vulns
-            sh '[ "${config.trivyFail}" == "NONE" ] || TRIVY_EXIT_CODE=1 TRIVY_SEVERITY=${config.trivyFail} make scan'
+            sh "[ \"${config.trivyFail}\" == \"NONE\" ] || TRIVY_EXIT_CODE=1 TRIVY_SEVERITY=${config.trivyFail} make scan"
           }
         }
 
