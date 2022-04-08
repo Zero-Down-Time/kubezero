dd0ef79dee353d98610010cd7b232dcbc31483ae
Stefan Reimer <stefan@zero-downtime.net>
2022-04-08 17:10:17 +0200

    feat: version bumps for 1.22 support

--- charts/kubezero-addons/update.sh
+++ charts/kubezero-addons/update.sh
@@ -3,9 +3,8 @@ set -ex
 
 NTH_VERSION=$(yq eval '.dependencies[] | select(.name=="aws-node-termination-handler") | .version' Chart.yaml)
 
-# Disabled until these AWS "pros" bump the chart number
-#rm -rf charts/aws-node-termination-handler
-#helm pull eks/aws-node-termination-handler --untar --untardir charts --version $NTH_VERSION
+rm -rf charts/aws-node-termination-handler
+helm pull eks/aws-node-termination-handler --untar --untardir charts --version $NTH_VERSION
 
 # diff -tuNr charts/aws-node-termination-handler.orig charts/aws-node-termination-handler > nth.patch
 patch -p0 -i nth.patch --no-backup-if-mismatch
