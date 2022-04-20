ee260b551a02896013bcf3c1b4b96091126620fa
Stefan Reimer <stefan@zero-downtime.net>
2022-04-21 00:57:45 +0200

    feat: Final touches and latest dashboards for Istio 1.13.3

--- charts/kubezero-istio/update.sh
+++ charts/kubezero-istio/update.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 set -ex
 
-### TODO
-# - https://istio.io/latest/docs/ops/configuration/security/harden-docker-images/
-
 export ISTIO_VERSION=$(yq eval '.dependencies[] | select(.name=="base") | .version' Chart.yaml)
 export KIALI_VERSION=$(yq eval '.dependencies[] | select(.name=="kiali-server") | .version' Chart.yaml)
 
 helm dep update
-
-# Patch
-#exit 0
-#diff -tubr istio istio.zdt/
-#patch -p0 -i zdt.patch --no-backup-if-mismatch
-
-# Create ingress charts
-#rm -rf ../kubezero-istio-ingress/charts/istio-*
-#cp -r istio/manifests/charts/gateways/istio-ingress ../kubezero-istio-ingress/charts/
-#cp -r istio/manifests/charts/gateways/istio-ingress ../kubezero-istio-ingress/charts/istio-private-ingress
-
-# Rename private chart
-#sed -i -e 's/name: istio-ingress/name: istio-private-ingress/' ../kubezero-istio-ingress/charts/istio-private-ingress/Chart.yaml
 
 # Get matching istioctl
 [ -x istioctl ] && [ "$(./istioctl version --remote=false)" == $ISTIO_VERSION ] || { curl -sL https://github.com/istio/istio/releases/download/${ISTIO_VERSION}/istioctl-${ISTIO_VERSION}-linux-amd64.tar.gz | tar xz; chmod +x istioctl; }
