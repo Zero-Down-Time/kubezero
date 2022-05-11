2404bfbbd90f546c87661cb49a7dbe87a327984c
Stefan Reimer <stefan@zero-downtime.net>
2022-05-11 16:31:37 +0200

    feat: Make Keycloak work behind Istio

--- charts/kubezero-auth/update.sh
+++ charts/kubezero-auth/update.sh
@@ -9,6 +9,6 @@ VERSION=$(yq eval '.appVersion' Chart.yaml)
 wget -q -O crds/keycloak.yaml https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${VERSION}/kubernetes/keycloaks.k8s.keycloak.org-v1.yml
  wget -q -O crds/keycloak-realmimports.yaml https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${VERSION}/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml
 
-wget -q -O templates/keycloak-operator/all.yaml https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${VERSION}/kubernetes/kubernetes.yml
+wget -q -O templates/keycloak/operator.yaml https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${VERSION}/kubernetes/kubernetes.yml
 
 patch -i keycloak.patch -p0 --no-backup-if-mismatch
