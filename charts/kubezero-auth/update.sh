8a3761b8137d3c805113ae24340cf19d79689e4d
Stefan Reimer <stefan@zero-downtime.net>
2022-05-11 13:08:56 +0200

    feat: Auth Keycloak operator working

--- charts/kubezero-auth/update.sh
+++ charts/kubezero-auth/update.sh
@@ -7,8 +7,7 @@ helm dep update
 VERSION=$(yq eval '.appVersion' Chart.yaml)
 
 wget -q -O crds/keycloak.yaml https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${VERSION}/kubernetes/keycloaks.k8s.keycloak.org-v1.yml
-# No realm imports needed so far
-# wget -q -O crds/keycloak-realmimport.yaml https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${VERSION}/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml
+ wget -q -O crds/keycloak-realmimports.yaml https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${VERSION}/kubernetes/keycloakrealmimports.k8s.keycloak.org-v1.yml
 
 wget -q -O templates/keycloak-operator/all.yaml https://raw.githubusercontent.com/keycloak/keycloak-k8s-resources/${VERSION}/kubernetes/kubernetes.yml
 
