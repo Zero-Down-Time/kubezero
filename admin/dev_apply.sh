ede7f022ee7d645e749d297d59b0c87d1982a1d4
Stefan Reimer <stefan@zero-downtime.net>
2023-08-16 10:17:39 +0000

    WIP: 1.26.7

---
+++ admin/dev_apply.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#set -eEx
+#set -o pipefail
+set -x
+
+#VERSION="latest"
+KUBE_VERSION="v1.26.6"
+WORKDIR=$(mktemp -p /tmp -d kubezero.XXX)
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# shellcheck disable=SC1091
+. "$SCRIPT_DIR"/libhelm.sh
+CHARTS="$(dirname $SCRIPT_DIR)/charts"
+
+get_kubezero_values
+
+# Always use embedded kubezero chart
+helm template $CHARTS/kubezero -f $WORKDIR/kubezero-values.yaml --kube-version $KUBE_VERSION --version ~$KUBE_VERSION --devel --output-dir $WORKDIR
+
+# CRDs first
+_helm crds $1
+_helm apply $1
