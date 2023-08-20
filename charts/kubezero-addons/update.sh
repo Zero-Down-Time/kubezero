17f5ae3f1b4dab1a1128b3c10b028b272009b72c
Stefan Reimer <stefan@zero-downtime.net>
2023-08-20 15:38:51 +0000

    Migrate addons to new update-lib

--- charts/kubezero-addons/update.sh
+++ charts/kubezero-addons/update.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 set -ex
 
-helm repo update
+. ../../scripts/lib-update.sh
 
-NTH_VERSION=$(yq eval '.dependencies[] | select(.name=="aws-node-termination-handler") | .version' Chart.yaml)
-RUH_VERSION=$(yq eval '.dependencies[] | select(.name=="aws-eks-asg-rolling-update-handler") | .version' Chart.yaml)
-
-rm -rf charts/aws-node-termination-handler
-helm pull eks/aws-node-termination-handler --untar --untardir charts --version $NTH_VERSION
-
-# diff -tuNr charts/aws-node-termination-handler.orig charts/aws-node-termination-handler > nth.patch
-patch -p0 -i nth.patch --no-backup-if-mismatch
-
-rm -rf charts/aws-eks-asg-rolling-update-handler
-helm pull twin/aws-eks-asg-rolling-update-handler --untar --untardir charts --version $RUH_VERSION
-patch -p0 -i ruh.patch --no-backup-if-mismatch
-
-helm dep update
+patch_chart aws-node-termination-handler
+patch_chart aws-eks-asg-rolling-update-handler
