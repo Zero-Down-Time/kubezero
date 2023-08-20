df9b85c1cf19c0400d9d076ee15ae584ba34bf4d
Stefan Reimer <stefan@zero-downtime.net>
2023-08-20 16:55:23 +0000

    Enable helm-values, adjust addons values.yaml

---
+++ scripts/lib-update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -ex
+
+#helm repo update
+
+# AWS public ECR
+aws ecr-public get-login-password \
+     --region us-east-1 | helm registry login \
+     --username AWS \
+     --password-stdin public.ecr.aws
+
+helm dep update
+
+patch_chart() {
+  CHART=$1
+
+  VERSION=$(yq eval '.dependencies[] | select(.name=="'$CHART'") | .version' Chart.yaml)
+
+  rm -rf charts/$CHART
+  tar xfvz charts/$CHART-$VERSION.tgz -C charts && rm charts/$CHART-$VERSION.tgz
+
+  # diff -tuNr charts/aws-node-termination-handler.orig charts/aws-node-termination-handler > nth.patch
+  patch -p0 -i $CHART.patch --no-backup-if-mismatch
+}
