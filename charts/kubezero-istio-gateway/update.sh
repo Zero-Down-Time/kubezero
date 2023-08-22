156f41fec6263d71c8e8a533e5c41614c2fc94ba
Stefan Reimer <stefan@zero-downtime.net>
2023-08-22 12:48:33 +0000

    Istio version bump for 1.26

--- charts/kubezero-istio-gateway/update.sh
+++ charts/kubezero-istio-gateway/update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ex
 
+. ../../scripts/lib-update.sh
+
+#login_ecr_public
+update_helm
+
 export ISTIO_VERSION=$(yq eval '.dependencies[] | select(.name=="gateway") | .version' Chart.yaml)
 
-helm dep update
-
-# Patch
-tar xf charts/gateway-$ISTIO_VERSION.tgz -C charts && rm -f charts/gateway-$ISTIO_VERSION.tgz
-#diff -tubr charts/gateway.orig charts/gateway
-patch -p0 -i zdt.patch --no-backup-if-mismatch
+patch_chart gateway
