8f89c3ce1422ef1280bb6ed45a334958b32f0640
Stefan Reimer <stefan@zero-downtime.net>
2023-08-23 12:20:50 +0000

    Version bump logging module for 1.26

--- charts/kubezero-logging/update.sh
+++ charts/kubezero-logging/update.sh
@@ -11,13 +11,17 @@ patch_chart eck-operator
 
 # fix ECK crds handling to adhere to proper helm v3 support which also fixes ArgoCD applying updates on upgrades
 mkdir charts/eck-operator/crds
-helm template charts/eck-operator/charts/eck-operator-crds --name-template logging > charts/eck-operator/crds/all-crds.yaml
+helm template charts/eck-operator/charts/eck-operator-crds --name-template logging --kube-version 1.26 > charts/eck-operator/crds/all-crds.yaml
 
 rm -rf charts/eck-operator/charts
 yq eval -Mi 'del(.dependencies)' charts/eck-operator/Chart.yaml
 
+# fluent-bit
+patch_chart fluent-bit
+
 # FluentD
 patch_chart fluentd
+rm -f charts/fluentd/templates/files.conf/systemd.yaml
 
 # Fetch dashboards from Grafana.com and update ZDT CM
 ../kubezero-metrics/sync_grafana_dashboards.py dashboards.yaml templates/fluent-bit/grafana-dashboards.yaml
