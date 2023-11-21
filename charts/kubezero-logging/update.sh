90e2ab60bcd084b16184c07fbff71f0f3912bafb
Stefan Reimer <stefan@zero-downtime.net>
2023-11-21 19:06:28 +0000

    Move eck-operator to new module

--- charts/kubezero-logging/update.sh
+++ charts/kubezero-logging/update.sh
@@ -3,18 +3,10 @@ set -ex
 
 . ../../scripts/lib-update.sh
 
-ECK_VERSION=$(yq eval '.dependencies[] | select(.name=="eck-operator") | .version' Chart.yaml)
+update_helm
+
 FLUENT_BIT_VERSION=$(yq eval '.dependencies[] | select(.name=="fluent-bit") | .version' Chart.yaml)
 FLUENTD_VERSION=$(yq eval '.dependencies[] | select(.name=="fluentd") | .version' Chart.yaml)
-
-patch_chart eck-operator
-
-# fix ECK crds handling to adhere to proper helm v3 support which also fixes ArgoCD applying updates on upgrades
-mkdir charts/eck-operator/crds
-helm template charts/eck-operator/charts/eck-operator-crds --name-template logging --kube-version 1.26 > charts/eck-operator/crds/all-crds.yaml
-
-rm -rf charts/eck-operator/charts
-yq eval -Mi 'del(.dependencies)' charts/eck-operator/Chart.yaml
 
 # fluent-bit
 patch_chart fluent-bit
