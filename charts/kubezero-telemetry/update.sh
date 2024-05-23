b2841271163dd61f0ec19b15e88efb3d85f0f837
Stefan Reimer <stefan@zero-downtime.net>
2024-05-23 21:18:15 +0000

    feat: add fluent-bit to new telemetry module, upgrade OpenSearch version ,various tweaks

--- charts/kubezero-telemetry/update.sh
+++ charts/kubezero-telemetry/update.sh
@@ -6,11 +6,11 @@ set -ex
 #login_ecr_public
 update_helm
 
-#FLUENT_BIT_VERSION=$(yq eval '.dependencies[] | select(.name=="fluent-bit") | .version' Chart.yaml)
+FLUENT_BIT_VERSION=$(yq eval '.dependencies[] | select(.name=="fluent-bit") | .version' Chart.yaml)
 FLUENTD_VERSION=$(yq eval '.dependencies[] | select(.name=="fluentd") | .version' Chart.yaml)
 
 # fluent-bit
-#patch_chart fluent-bit
+patch_chart fluent-bit
 
 # FluentD
 patch_chart fluentd
