0f5d7ea968f883ec6a09ad1f964dd62e2a2a05c2
Stefan Reimer <stefan@zero-downtime.net>
2024-06-10 17:00:29 +0000

    fix: improved crd handling, version bumps of network, new HAProxy support, argocd auth fix

--- charts/kubezero-network/update.sh
+++ charts/kubezero-network/update.sh
@@ -7,6 +7,7 @@ set -ex
 update_helm
 
 # Create ZDT dashboard configmap
-../kubezero-metrics/sync_grafana_dashboards.py dashboards.yaml templates/grafana-dashboards.yaml
+../kubezero-metrics/sync_grafana_dashboards.py cilium-dashboards.yaml templates/cilium-grafana-dashboards.yaml
+../kubezero-metrics/sync_grafana_dashboards.py haproxy-dashboards.yaml templates/haproxy-grafana-dashboards.yaml
 
 update_docs
