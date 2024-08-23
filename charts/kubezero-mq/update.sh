5f01d4ec852ddc4bd7105a926e27b14d8552410a
Stefan Reimer <stefan@zero-downtime.net>
2024-08-23 14:56:24 +0000

    feat: Major version bump for all MQ components

--- charts/kubezero-mq/update.sh
+++ charts/kubezero-mq/update.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+##!/bin/bash
 set -ex
 
-helm dep update
+. ../../scripts/lib-update.sh
 
-## NATS
-
-NATS_VERSION=0.8.4
-rm -rf charts/nats && curl -L -s -o - https://github.com/nats-io/k8s/releases/download/v$NATS_VERSION/nats-$NATS_VERSION.tgz | tar xfz - -C charts
+#login_ecr_public
+update_helm
 
 # Fetch dashboards
 ../kubezero-metrics/sync_grafana_dashboards.py dashboards-nats.yaml templates/nats/grafana-dashboards.yaml
 ../kubezero-metrics/sync_grafana_dashboards.py dashboards-rabbitmq.yaml templates/rabbitmq/grafana-dashboards.yaml
+
+update_docs
